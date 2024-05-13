2f46ae3470a28c6a24b8ad1836bfb197ec463d67
mkarmark <mikarmar@microsoft.com>
2024-05-13 14:08:16 -0700

    Update index.js

--- api/GetRoles/index.js
+++ api/GetRoles/index.js
@@ -2,4 +2,8 @@ module.exports = async function (context, req) {
     const user = req.body || {};
     const roles = [];
     roles.push('customrole');
+
+    context.res.json({
+        roles
+    });
 }
